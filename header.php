4e232009966d05a186396325b0b179205044780d
Forrest Wilson <forrest.wilson@me.com>
2018-05-12 15:58:16 +1200

    Header styling

--- header.php
+++ header.php
@@ -22,6 +22,33 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	
+
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(1, 66, 130);">
+			<div class="container">
+				<a href="#" class="navbar-brand">
+					<img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/cat-and-dog.gif" height="100" width="100" />
+					<img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/humane-society.gif" style="max-width: 300px" />
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<?php 
+					wp_nav_menu(array(
+						"theme_location" => "primary",
+						"container" => "div",
+						"container_id" => "navbarNavDropdown",
+						"container_class" => "navbar-collapse collapse justify-content-end",
+						"menu_class" => "navbar-nav",
+						"depth" => 2,
+						"walker" => new WP_Bootstrap_Navwalker()
+					));
+				?>
+			</div>
+		</nav>
+
+		
+	</header>	
 
 	<div id="content" class="site-content">
