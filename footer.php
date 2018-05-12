4de9d8bcbb9cc199a5c36e99bb20a28621c9b5a5
Forrest Wilson <forrest.wilson@me.com>
2018-05-12 15:58:10 +1200

    Basic footer styling

--- footer.php
+++ footer.php
@@ -14,18 +14,20 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humane-society' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'humane-society' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'humane-society' ), 'humane-society', '<a href="http://underscores.me/">Forrest Wilson-Jennings</a>' );
-				?>
+		<div class="site-info" style="padding: 50px 25px; background-color: rgb(1, 66, 130); color: white;">
+			<div class="container">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humane-society' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'humane-society' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'humane-society' ), 'humane-society', '<a href="https://forrest-wilson.com" target="_blank">Forrest Wilson-Jennings</a>' );
+					?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
