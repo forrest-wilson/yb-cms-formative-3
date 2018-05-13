97beac0148f04d94a12de5ee6414cb1d43286fad
Forrest Wilson <forrest.wilson@me.com>
2018-05-14 00:13:55 +1200

    Added sidebar to the home page

--- page-home.php
+++ page-home.php
@@ -10,7 +10,14 @@
     <main id="main" class="site-main">
         <?php get_template_part("template-parts/home", "hero"); ?>
         <section class="container">
-            <?php the_content(); ?>
+            <div class="row">
+                <div class="col-sm-12 col-md-8 col-lg-9">
+                    <?php the_content(); ?>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-3">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
         </section>
     </main>
 </div>
